71110fbe154e8bb362895a7f79078671e865403d
Your Name <your.email@example.com>
2025-04-09 15:50:33 +0800

    移除 server/plugins/mongodb.ts 以解決靜態部署錯誤

--- server/plugins/mongodb.ts
+++
@@ -1,6 +0,0 @@
-// import { connectToMongoDB } from '../utils/mongodb';
-// import { defineNitroPlugin } from 'nitropack/runtime/plugin';
-
-// export default defineNitroPlugin(async () => {
-//     await connectToMongoDB();
-// }); 
